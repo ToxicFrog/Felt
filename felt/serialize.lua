4e055def75d479749265785d6020b8de3444258e
Ben Kelly <ben@leela.(none)>
2010-10-14 11:06:35 -0400

    New serializer complete

--- felt/serialize.lua
+++ felt/serialize.lua
@@ -1,6 +1,20 @@
---require "net"
+--[[
+serialization format
+A serialized message consists of any number of concatenated items.
+
+nil		:= 'n'
+true	:= 't'
+false	:= 'f'
+number	:= 'N' value
+string	:= 'S' size value
+table	:= 'T' size (item item)*
+no function
+no userdata
+no thread
+]]
 
 local repr = {}
+
 
 local function serialize(...)
     if select('#', ...) == 0 then return "" end
@@ -18,46 +32,42 @@ local function serialize(...)
     end)(...)
 end
 
---[[
-local function net_serialize(v, ...)
-    if v == nil and select('#', ...) == 0 then return "" end
-    
-    if getmetatable(v) and getmetatable(v).__send then
-        return getmetatable(v).__send(v)..net.serialize(...)
-    end
-    
-    if repr[type(v)] then
-        return repr[type(v)](v, net.serialize)..net.serialize(...)
-    end
-    
-    return error("Cannot serialize type '"..type(v).."'")
-end
---]]
-
-function repr.string(s)
-    return string.format("S%08d%s", #s, s)
-end
-
-function repr.number(n)
-    return string.format("N%08d%s", #tostring(n), tostring(n))
+repr["nil"] = function()
+	return "n"
 end
 
 function repr.boolean(b)
-    return b and "Bt" or "Bf"
+	return b and "t" or "f"
 end
 
-function repr.table(t, f)
-    local buf = { "T" }
-    for k,v in pairs(t) do
-        buf[#buf+1] = (f or serialize)(k)
-        buf[#buf+1] = (f or serialize)(v)
-    end
-    buf[#buf+1] = "t"
-    
-    return table.concat(buf, "")
+function repr.number(n)
+	return "N"..tostring(n)..":"
 end
 
-repr["nil"] = function() return "." end
+function repr.string(s)
+	return "S"..tostring(#s)..":"..s
+end
 
+local indent = ""
+function repr.table(t)
+	local n = 0
+	local buf = {}
+	
+	indent = indent.."    "
+	
+	for k,v in pairs(t) do
+		n = n+1
+		local key = serialize(k)
+		local value = serialize(v)
+		table.insert(buf, indent)
+		table.insert(buf, key)
+		table.insert(buf, "\t")
+		table.insert(buf, value)
+		table.insert(buf, "\n")
+	end
+	
+	indent = indent:sub(1,-5)
+	return "T"..tostring(n)..":".."\n"..table.concat(buf)
+end
 felt.serialize = serialize
 
