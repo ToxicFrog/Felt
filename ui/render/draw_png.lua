7021a12a62cc6b80d626292c51385f40448d4b1d
Ben Kelly <bkelly@uoguelph.ca>
2011-03-02 14:31:45 -0500

    ui.render.draw_png now tracks changes to self.face, loading new PNGs from disk as needed

--- ui/render/draw_png.lua
+++ ui/render/draw_png.lua
@@ -1,26 +1,30 @@
 -- Tokens that can draw front and back faces from a PNG image
 -- back face is optional and defaults to front face
 
+local surfaces = setmetatable({}, { __index = function(self, name)
+	self[name] = assert(cairo.image_surface_create_from_png(name))
+	return self[name]
+end})
+
 local _init = __init
 function __init(self, ...)
 	_init(self, ...)
 	
 	assert(self.face, "No face for PNG surface")
-	self.surface = cairo.image_surface_create_from_png(self.face)
-    self.back_surface = cairo.image_surface_create_from_png(self.back or self.face)
+	self.back = self.back or self.face
 	
-	self.w = self.w or cairo.image_surface_get_width(self.surface)
-	self.h = self.h or cairo.image_surface_get_height(self.surface)
+	self.w = self.w or cairo.image_surface_get_width(surfaces[self.face])
+	self.h = self.h or cairo.image_surface_get_height(surfaces[self.face])
 end
 
 function draw(self, cr)
-	cr:set_source_surface(self.surface, 0, 0)
+	cr:set_source_surface(surfaces[self.face], 0, 0)
 	cr:rectangle(0, 0, self.w, self.h)
 	cr:fill()
 end
 
 function drawBack(self, cr)
-	cr:set_source_surface(self.back_surface, 0, 0)
+	cr:set_source_surface(surfaces[self.back], 0, 0)
 	cr:rectangle(0, 0, self.w, self.h)
 	cr:fill()
 end
