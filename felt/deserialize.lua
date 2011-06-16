62ea7465aa761204d65eb92c8620cf5dcc192bc4
Ben Kelly <bkelly@uoguelph.ca>
2011-06-16 14:34:28 -0400

    Removed old (de)serialization libraries.

--- felt/deserialize.lua
+++
@@ -1,8 +0,0 @@
-function felt.deserialize(buf)
-	local dc = new "Deserialization" {
-		data = buf;
-		game = felt.game;
-	}
-	
-	return dc:unpack()
-end
--- felt/serialize.lua
+++
@@ -1,9 +0,0 @@
-function felt.serialize(...)
-	local sc = new "Serialization" {}
-	
-	for i=1,select('#', ...) do
-		sc:pack((select(i, ...)))
-	end
-	
-	return sc:finalize()
-end
